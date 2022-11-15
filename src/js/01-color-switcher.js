11050c014b644f157fe8bceb00806076cbfd5085
beligrous <beligrous@gmail.com>
2022-11-15 23:33:02 +0200

    begin task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+const colorizer = {
+  start() {
+    const intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+  },
+};
+
+function onStartClick() {
+  colorizer.start;
+}
+
+function onStopClick() {
+  colorizer.stop;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
